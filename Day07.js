eb57dd0d86d94e641df4a48f645afa08c5c3c07b
Vinu02 <107649664+Vinu02@users.noreply.github.com>
2022-08-05 00:28:24 +0530

    Add files via upload

---
+++ Day07.js
@@ -0,0 +1,31 @@
+var request=new XMLHttpRequest();
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+request.send();
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    console.log(result);
+
+ //print the all the countries from Asia continent/region using the filter function
+
+  
+ var res=result.filter((ele)=>ele.region==="Asia");
+ console.log(res);
+
+
+//print all the countries with a populatio of less than 2LAKHs using filter function
+  
+  var res=result.filter((ele)=>ele.population<200000);
+  console.log(res);
+
+//print the total population of countries using reduce function
+
+var res=result.reduce((acc,ele)=>acc+ele.population,0);
+console.log(res);
+
+//print the country which uses US Dollers as currency
+//var res=result.filter((ele)=>ele.USD==="currency");
+//console.log(res);
+ 
+
+
+}
